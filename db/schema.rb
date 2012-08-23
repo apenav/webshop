878c4a3debedee0717165d12d779482813425be9
Antonio Peña <apenav@desde.asia>
2012-08-23 21:25:10 +0200

    Gemfile +bcrypt; rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120823121425) do
+ActiveRecord::Schema.define(:version => 20120823191924) do
 
   create_table "products", :force => true do |t|
     t.string   "brand"
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(:version => 20120823121425) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
