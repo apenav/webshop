fd70fa69b0313a2d9c9bd1c0bc351f52c7fd94f4
Antonio Peña <apenav@desde.asia>
2012-08-24 17:50:27 +0200

    CALLBACK counter_cache (on reviews) instead of reviews_count (on products)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,14 +1,16 @@
 class Review < ActiveRecord::Base
   attr_accessible :description, :rating, :summary
  
-  after_create :increment_reviews_count
+  #after_create :increment_reviews_count
 
-  def increment_reviews_count
-  	#product.reviews_count += 1
-  	#product.save
-  	product.increment! :reviews_count # equivalentes; importante el (!) para que gaurde en bd
-  end
+  # def increment_reviews_count
+  # 	#product.reviews_count += 1
+  # 	#product.save
+  # 	product.increment! :reviews_count # equivalentes; importante el (!) para que gaurde en bd
+  # end
 
-  belongs_to :product
+  belongs_to :product, :counter_cache => true
+  #belongs_to :product, :counter_cache => :reviews_count
+  
   belongs_to :user
 end
