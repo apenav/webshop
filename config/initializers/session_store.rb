942bde2668e7711a5ecda104e795dc67cf651ab7
Antonio Peña <apenav@desde.asia>
2012-08-25 17:37:03 +0200

    security

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Webshop::Application.config.session_store :cookie_store, key: '_webshop_session'
+Webshop::Application.config.session_store :cookie_store,
+                                          key: '_webshop_session',
+                                          secure: ::APP_CONFIG.secure_cookie
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
